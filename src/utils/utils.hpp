fa5c8ab83ccd56467f25afe7804337d05672c524
Arash Hatami <hatamiarash7@gmail.com>
2025-02-11 18:14:41 +0330

    chore: export LogMessage function

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -6,6 +6,9 @@ namespace duckdb
 {
 	namespace netquack
 	{
+		// Function to log messages with a specified log level
+		void LogMessage(const std::string &level, const std::string &message);
+
 		// Function to download a file from a URL
 		size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 
